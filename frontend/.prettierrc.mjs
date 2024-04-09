b2aec62dd1e0817e1ec10cd872f2738548453c5a
Heaven Chen <891118heaven@gmail.com>
2024-04-09 08:59:25 +0800

    [Add] Add prettier and eslint

---
+++ frontend/.prettierrc.mjs
@@ -0,0 +1,32 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: ['src/**/*.astro'],
+      options: {
+        parser: 'astro',
+        arrowParens: 'avoid',
+        singleQuote: true,
+        bracketSpacing: true,
+        endOfLine: 'lf',
+        semi: false,
+        tabWidth: 2,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: ["*.js", "*.ts", "*.tsx", "*.jsx"],
+      options: {
+        arrowParens: 'avoid',
+        singleQuote: true,
+        bracketSpacing: true,
+        endOfLine: 'lf',
+        semi: false,
+        tabWidth: 2,
+        trailingComma: 'none',
+      },
+    }
+  ],
+};
