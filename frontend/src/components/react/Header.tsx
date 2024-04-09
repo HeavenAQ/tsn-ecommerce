b06a01a271240da4be532d03a64b1f29495aaa93
Heaven Chen <891118heaven@gmail.com>
2024-04-09 11:18:40 +0800

    [Update] Change tailwind config to allow custom classes

---
+++ frontend/src/components/react/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <h3 className="py-4 px-10">
+      HEADER 
+    </h3>
+  )
+}
---
+++ frontend/src/components/react/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <h3 className="row-span-2 py-12 px-9">
+      SideBar
+    </h3>
+  )
+}
